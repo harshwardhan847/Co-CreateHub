fef504bcf6a2b687d4b4ea58dd00d2978d369f77
Harsh <harshwardhan847@gmail.com>
2023-06-04 09:20:45 +0530

    removed scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -192,7 +192,7 @@ const Home = () => {
                 </svg>
               </button>
 
-              <button
+              {/* <button
                 type="button"
                 onClick={showNotification}
                 data-dropdown-toggle="notification-dropdown"
@@ -209,7 +209,7 @@ const Home = () => {
                   <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
                 </svg>
               </button>
-              <NotificationDropdown show={dropdown.notification} />
+              <NotificationDropdown show={dropdown.notification} /> */}
 
               <button
                 type="button"
